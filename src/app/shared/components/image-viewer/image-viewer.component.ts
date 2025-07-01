5004e4260ddc295c45c847b4cbc48186194e9eab
Stanislav Stolbennikov <stolbennikovilmk@gmail.com>
2025-07-01 16:17:44 +0700

    Added main logic for image-viewer.component

--- src/app/shared/components/image-viewer/image-viewer.component.ts
+++ src/app/shared/components/image-viewer/image-viewer.component.ts
@@ -1,5 +1,4 @@
 import {
-    AfterViewInit,
     ChangeDetectionStrategy,
     ChangeDetectorRef,
     Component,
@@ -9,39 +8,58 @@ import {
     HostListener,
     inject,
     OnDestroy,
-    OnInit,
-    ViewChild,
+    OnInit, Renderer2,
+    viewChild,
 } from '@angular/core';
 import { debounceTime, fromEvent } from 'rxjs';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { ImageViewerService } from './image-viewer.service';
 import { ImageViewerConfig } from './image-viewer.config';
+import { TagComponent } from '@components/tag/tag.component';
+
+const SELECTED_INDEX_VISIBILITY_TIME = 2000;
 
 @Component({
     selector: 'bc-image-viewer',
     standalone: true,
-    imports: [],
+    imports: [
+        TagComponent,
+    ],
     templateUrl: './image-viewer.component.html',
     styleUrl: './image-viewer.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ImageViewerComponent implements OnInit, AfterViewInit, OnDestroy {
+export class ImageViewerComponent implements OnInit, OnDestroy {
+    protected readonly Math = Math;
+
     public readonly config = inject(ImageViewerConfig);
     private readonly cd = inject(ChangeDetectorRef);
     private readonly imageViewerService = inject(ImageViewerService);
+    private readonly renderer2 = inject(Renderer2);
     private readonly destroyRef = inject(DestroyRef);
 
     public componentRef: ComponentRef<any> | null = null;
+    public selectedMediaIndex = this.config.selectedMediaIndex;
 
     private _resizeObserver: ResizeObserver | null = null;
+    private _selectedIndexAnimationTimeoutId: number | null = null;
 
-    @ViewChild('imageViewer')
-    imageViewer!: ElementRef;
+    public imageViewer = viewChild.required('imageViewer');
+    public selectedIndex = viewChild.required('selectedIndex', { read: ElementRef });
 
-
-    @HostListener('document:keydown.escape')
-    public onKeydownHandler() {
-        this.closeModal();
+    @HostListener('document:keydown', ['$event'])
+    public onKeydownHandler(event: KeyboardEvent) {
+        switch (event.key) {
+            case 'Escape':
+                this.closeModal();
+                break;
+            case 'ArrowLeft':
+                this.switchMedia('left');
+                break;
+            case 'ArrowRight':
+                this.switchMedia('right');
+                break;
+        }
     }
 
     public ngOnInit(): void {
@@ -51,14 +69,6 @@ export class ImageViewerComponent implements OnInit, AfterViewInit, OnDestroy {
                 takeUntilDestroyed(this.destroyRef),
             )
             .subscribe();
-    }
-
-    public ngAfterViewInit(): void {
-        requestAnimationFrame(() => {
-            this.imageViewer.nativeElement.classList.add('transition');
-        });
-
-        this.cd.detectChanges();
     }
 
     public closeModal(): void {
@@ -72,5 +82,28 @@ export class ImageViewerComponent implements OnInit, AfterViewInit, OnDestroy {
 
         this._resizeObserver?.disconnect();
         this._resizeObserver = null;
+    }
+
+    private switchMedia(direction: 'left' | 'right') {
+        if (direction === 'left') {
+            this.selectedMediaIndex = Math.max(0, this.selectedMediaIndex - 1);
+        }
+
+        if (direction === 'right') {
+            this.selectedMediaIndex = Math.min(this.config.media.length - 1, this.selectedMediaIndex + 1);
+        }
+
+        const selectedIndexEl = this.selectedIndex().nativeElement;
+
+        this.renderer2.addClass(selectedIndexEl, 'visible');
+
+        if (this._selectedIndexAnimationTimeoutId) {
+            clearTimeout(this._selectedIndexAnimationTimeoutId);
+        }
+
+        this._selectedIndexAnimationTimeoutId = setTimeout(() => {
+            this.renderer2.removeClass(selectedIndexEl, 'visible');
+            this._selectedIndexAnimationTimeoutId = null;
+        }, SELECTED_INDEX_VISIBILITY_TIME);
     }
 }
