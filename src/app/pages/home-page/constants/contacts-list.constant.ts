37ff5915cdad83a176484440d4f651211ee922ac
Stanislav Stolbennikov <stolbennikovilmk@gmail.com>
2026-03-20 02:49:08 +0700

    Added LinkedIn link

--- src/app/pages/home-page/constants/contacts-list.constant.ts
+++ src/app/pages/home-page/constants/contacts-list.constant.ts
@@ -3,6 +3,7 @@ import { IconMailComponent } from '@icons/icon-mail/icon-mail.component';
 import { IconTelegramComponent } from '@icons/icon-telegram/icon-telegram.component';
 import { IconGithubComponent } from '@icons/icon-github/icon-github.component';
 import { IconInstagramComponent } from '@icons/icon-instagram/icon-instagram.component';
+import { IconLinkedinComponent } from '@icons/icon-linkedin/icon-linkedin.component';
 
 export const CONTACTS_LIST: IIconsLinks[] = [
     {
@@ -19,6 +20,11 @@ export const CONTACTS_LIST: IIconsLinks[] = [
         alt: 'Github',
         link: 'https://github.com/SSE-programmer',
         icon: IconGithubComponent,
+    },
+    {
+        alt: 'LinkedIn',
+        link: 'https://www.linkedin.com/in/stanislav-stolbennikov-a95226228',
+        icon: IconLinkedinComponent,
     },
     {
         alt: 'Instagram',
---
+++ src/app/shared/icons/icon-linkedin/icon-linkedin.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+    selector: 'bc-icon-linkedin',
+    templateUrl: './icon-linkedin.component.html',
+    styleUrl: './icon-linkedin.component.scss',
+    imports: [],
+    changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class IconLinkedinComponent {
+}
